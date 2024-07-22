3556cd7b094f065bc35a0ce59baf9d04db6b770e
romulo.araujo <romulo.araujo@acocearense.com.br>
2024-07-22 17:24:01 -0300

    ajuste

--- src/pages/Footer/index.jsx
+++ src/pages/Footer/index.jsx
@@ -1,65 +1,82 @@
-import React from 'react';
 import styled from 'styled-components';
 import logo from '../../assets/logo-footer.svg';
 import rede1 from '../../assets/facebook.svg';
 import rede2 from '../../assets/instagram.svg';
 import rede3 from '../../assets/twitter.svg';
 
-
 const StyleFooter = styled.footer`
-
+color: white;
 background-color: #000000;
 display: flex;
-color: #ffffff;
-width: 100%;
-height: auto;
+justify-content: center;
 
-.textcontent{
-    text-align: justify;
+.div1{
+    width: 100%;
+    height: 200px;
+    align-content: center;
+}
+.div1 h2{
+    color: #c7cacc;
+    display: flex;
+    align-content: center;
+    justify-content: center;
 }
 `;
 
 const Footer = () => {
     return (<>
         <StyleFooter>
-            <div className='grid align-items-center justify-content-around '>
-                <div className='col-2 grid '>
-                    <div className='col-6'>
+            <div className='grid justify-content-center'>
+                <div className='col-8 '>
+                    <div className='col text-center p-3 border-round-sm bg-primary font-bold'>
                         <img src={logo} alt="logo" />
                     </div>
-                    <div className='col-12 textcontent'>
-
-                        vitae excepturi quo voluptatum cupiditate?
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-
+                    <div className='col text-left text-center p-3 border-round-sm bg-primary font-bold'>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia consequatur quo doloribus ducimus nulla? Explicabo quas
+                        blanditiis aliquid rerum deserunt ab officia modi culpa nobis, sequi quasi dignissimos maxime dolores.
                     </div>
-                    <div className='col-12 flex justify-content-around'>
-                        <div>
-                            <img src={rede1} alt="rede1" />
+                    <div className='grid col text-center p-3 border-round-sm bg-primary font-bold'>
+                        <div className='col-2'>
+                            <img src={rede1} alt="facebook" />
                         </div>
-                        <div>
-                            <img src={rede2} alt="rede2" />
+                        <div className='col-2'>
+                            <img src={rede2} alt="instagram" />
                         </div>
-                        <div>
-                            <img src={rede3} alt="rede3" />
+                        <div className='col-2'>
+                            <img src={rede3} alt="twitter" />
                         </div>
                     </div>
                 </div>
-                <div className='col-2  w-2 grid'>
-                    <div className='col-12 textcontent'>
-                        vitae excepturi quo voluptatum cupiditate?
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore eum velit a quaerat
-                        vitae excepturi quo voluptatum cupiditate?
+            </div>
+            <div className='grid'>
+                <div className='col-8 '>
+                <div className='col text-left text-center p-3 border-round-sm bg-primary font-bold'>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia consequatur quo doloribus ducimus nulla? Explicabo quas
+                        blanditiis aliquid rerum deserunt ab officia modi culpa nobis, sequi quasi dignissimos maxime dolores.
+                    </div>
+                    
+                    <div className='col text-left text-center p-3 border-round-sm bg-primary font-bold'>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia consequatur quo doloribus ducimus nulla? Explicabo quas
+                        blanditiis aliquid rerum deserunt ab officia modi culpa nobis, sequi quasi dignissimos maxime dolores.
                     </div>
 
                 </div>
-                <div className='col-4 grid'>
-                    <div className='col-12 textcontent'>
-                        Lorem ipsum dolor sit amet consectetur
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        vitae excepturi quo voluptatum cupiditate?
+
+            </div>
+            <div className='grid'>
+                <div className='col-8'>
+                <div className='col text-left text-center p-3 border-round-sm bg-primary font-bold'>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia consequatur  
+                        i quasi dignissimos maxime dolores.
                     </div>
+                    
+                    <div className='col text-left text-center p-3 border-round-sm bg-primary font-bold'>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia consequatur quo 
+                        modi culpa nobis, sequi quasi dignissimos maxime dolores.
+                    </div>
+
                 </div>
+
             </div>
         </StyleFooter>
     </>);
